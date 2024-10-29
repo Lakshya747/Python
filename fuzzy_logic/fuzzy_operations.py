2e4b9fe8c6318200c49b2e1bafd3342c4c079b59
Lakshya747 <lakshya.agarwal747@gmail.com>
2024-10-29 22:33:48 +0530

    Update fuzzy_operations.py

--- fuzzy_logic/fuzzy_operations.py
+++ fuzzy_logic/fuzzy_operations.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-
 import matplotlib.pyplot as plt
 import numpy as np
+
 @dataclass
 class FuzzySet:
     """
@@ -15,7 +15,7 @@ class FuzzySet:
     right_boundary: float
 
     def __str__(self) -> str:
-        return f"{self.name}:[{self.left_boundary}, {self.peak}, {self.right_boundary}]"
+        return f"{self.name}: [{self.left_boundary}, {self.peak}, {self.right_boundary}]"
 
     def complement(self) -> FuzzySet:
         return FuzzySet(
@@ -39,7 +39,8 @@ class FuzzySet:
         elif self.left_boundary < x <= self.peak:
             return (x - self.left_boundary) / (self.peak - self.left_boundary)
         elif self.peak < x < self.right_boundary:
-            return (self.right_boundary - x) / (self.right_boundary - self.peak) 
+            return (self.right_boundary - x) / (self.right_boundary - self.peak)
+        
         msg = f"Invalid value {x} for fuzzy set {self}"
         raise ValueError(msg)
 
